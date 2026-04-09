c0b48503c5d5c4771c2b2f6d332be7ef1e88ef60
Se Ho <150892874+seho0726@users.noreply.github.com>
2026-04-09 16:11:33 +0900

    [level 2] Title: 무인도 여행, Time: 13.61 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,36 +1,40 @@
-import sys
-sys.setrecursionlimit(10000)
+from collections import deque
 
 def solution(maps):
-
+    
     n = len(maps)
     m = len(maps[0])
     answer = []
-    
     visited = [[False] * m for _ in range(n)]
+    
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
+    
+    def bfs(r, c):
+        q = deque([(r, c)])
+        visited[r][c] = True
+        total = int(maps[r][c])
+        
+        while q:
+            x, y = q.popleft()
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
                 
-    def dfs(x, y):
-        
-        if y < 0 or y >= m or x < 0 or x >= n or visited[x][y] or maps[x][y] == 'X':
-            return 0
-        
-        visited[x][y] = True
-
-        total = int(maps[x][y])
-        total += dfs(x+1, y)
-        total += dfs(x-1, y)
-        total += dfs(x, y+1)
-        total += dfs(x, y-1)
+                if 0 <= nx < n and 0 <= ny < m:
+                    if maps[nx][ny] != 'X' and not visited[nx][ny]:
+                        visited[nx][ny] = True
+                        total += int(maps[nx][ny])
+                        q.append((nx, ny))
         return total
-        
-        
+    
     for a in range(n):
         for b in range(m):
             if maps[a][b] != 'X' and not visited[a][b]:
-                res = dfs(a, b)
-                if res > 0:
-                    answer.append(res)
-
+                answer.append(bfs(a, b))
+                
     if not answer:
         return [-1]
+    
     return sorted(answer)
