b4f5ae2ec27580c61daa2efefd0a344187b891b7
Se Ho <150892874+seho0726@users.noreply.github.com>
2026-04-09 14:30:28 +0900

    [level 2] Title: 호텔 대실, Time: 3.59 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,25 +1,3 @@
-import heapq
-
-def trans_time(time_str):
-    h, m = map(int, time_str.split(':'))
-    return 60 * h + m
-
 def solution(book_time):
-    
-    # 시간 변환 -> 분으로 변환
-    for a in range(len(book_time)):
-        for b in range(2):
-            book_time[a][b] = trans_time(book_time[a][b])
-    
-    book_time.sort()
-    room = []
-    
-    for start, end in book_time:
-        
-        if room and room[0] <= start:
-            heapq.heappop(room)
-        
-        heapq.heappush(room, end + 10)
-    
-    answer = len(room)
+    answer = 0
     return answer
