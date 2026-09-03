e4bf36f41ac76e0be50a64a0eb8c237c54fdf074
Se Ho <150892874+seho0726@users.noreply.github.com>
2026-09-04 01:34:27 +0900

    [level 2] Title: 광물 캐기, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -3,15 +3,17 @@ def solution(picks, minerals):
     minerals = minerals[:total_picks * 5]
     
     groups = []
+    
     for i in range(0, len(minerals), 5):
         chunk = minerals[i:i+5]
         groups.append([chunk.count("diamond"), chunk.count("iron"), chunk.count("stone")])
     
     groups.sort(key= lambda x : (-x[0], -x[1], -x[2]))
     answer = 0
+    
     for g in groups:
         dia, iron, stone = g
-            
+        
         if picks[0] > 0:
             answer += (dia + iron + stone)
             picks[0] -= 1
@@ -21,5 +23,5 @@ def solution(picks, minerals):
         elif picks[2] > 0:
             answer += (dia*25 + iron*5 + stone)
             picks[2] -= 1
-            
+
     return answer
